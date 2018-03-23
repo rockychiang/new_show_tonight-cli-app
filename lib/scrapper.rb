4c456925cb2feee946e5fd2e74a2d66464725f61
Rocky Chiang <rocky@chem.ucla.edu>
2018-03-23 02:54:27 +0000

    added show class with initialize, save and self.all method

--- lib/scrapper.rb
+++ lib/scrapper.rb
@@ -1,2 +1,5 @@
 class Scrapper
+  def self.list_scrapper
+    url = Nokogiri::HTML(open("https://next-episode.net/recent/"))
+  end
 end
--- lib/show.rb
+++ lib/show.rb
@@ -1,3 +1,16 @@
 class Show
+  attr_accessor :name, :summary, :genre, :channel, :showtime, :season, :episode, :status
+  @@all = []
 
+  def initialize
+    @genre = []
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
 end
